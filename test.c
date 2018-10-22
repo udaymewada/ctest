27e57943b858a9d465e016f56444c54c91077a21
Uday Mewada <udaymewada1@gmail.com>
2018-10-23 00:08:03 +0530

    looks more readable
    few documentation fixes

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-#include<stdio.h>	//header file
-int main()	//main funtion (it needs to be included in every program)
+#include<stdio.h>			//header file (pre processor)
+int main()				//main funtion (it needs to be included in every program)
 {
 	printf("Hello world !!!");	//print "Hello world !!!" in output screen
 	return 0;			//return nothing
