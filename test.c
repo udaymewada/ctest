fe56dee3070591022e155995d3766d5c10706e17
Hariom Verma <37576387+harry-hov@users.noreply.github.com>
2018-10-23 00:06:16 +0530

    documented

--- test.c
+++ test.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>	//header file
+int main()	//main funtion (it needs to be included in every program)
 {
-	printf("Hello world !!!");
-	return 0;
-	
+	printf("Hello world !!!");	//print "Hello world !!!" in output screen
+	return 0;			//return nothing
 }
